ba5f0d53b9735a01be18d2155870bf3a89bbb23f
Virain Garg <virain.garg@ad.infosys.com>
2019-05-30 13:29:11 +0530

    Updates

---
+++ precache-manifest.7021b21a913efa60780a897788a2876e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa4ddf1333cbb4e49f844bad47990c6",
+    "url": "/react_draggable/public/index.html"
+  },
+  {
+    "revision": "567c2ab33db8e2fb5b59",
+    "url": "/react_draggable/public/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "f71bdb25ec7679479747",
+    "url": "/react_draggable/public/static/css/main.66d4a23a.chunk.css"
+  },
+  {
+    "revision": "567c2ab33db8e2fb5b59",
+    "url": "/react_draggable/public/static/js/2.ca656cb4.chunk.js"
+  },
+  {
+    "revision": "f71bdb25ec7679479747",
+    "url": "/react_draggable/public/static/js/main.c8b4d976.chunk.js"
+  },
+  {
+    "revision": "bcfd6168346523917a8c",
+    "url": "/react_draggable/public/static/js/runtime~main.ce498a39.js"
+  }
+]);
