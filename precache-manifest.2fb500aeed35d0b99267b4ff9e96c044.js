1539972febd3cd677b1d28e61c1367e82dd7b4ff
Virain Garg <virain.garg@ad.infosys.com>
2019-05-30 13:39:03 +0530

    Updates

--- precache-manifest.2fb500aeed35d0b99267b4ff9e96c044.js
+++ precache-manifest.2fb500aeed35d0b99267b4ff9e96c044.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f27edeb208c719fd2e77a1dc8b4bba01",
+    "revision": "59317765aaf74d810d059ec6898cf4ab",
     "url": "/react_draggable/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_draggable/static/css/2.4364cc03.chunk.css"
   },
   {
-    "revision": "f71bdb25ec7679479747",
+    "revision": "ac0f45fc294b2eb70323",
     "url": "/react_draggable/static/css/main.66d4a23a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_draggable/static/js/2.ca656cb4.chunk.js"
   },
   {
-    "revision": "f71bdb25ec7679479747",
-    "url": "/react_draggable/static/js/main.c8b4d976.chunk.js"
+    "revision": "ac0f45fc294b2eb70323",
+    "url": "/react_draggable/static/js/main.561ad9bf.chunk.js"
   },
   {
     "revision": "5c79f915eb4c36ca15df",
